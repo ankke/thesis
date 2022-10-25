636d53c64aa9586641f9cefa36075000dd414729
Alexander Berger <alexander_berger@c1-mgmt.aim.in.tum.de>
2022-10-25 13:28:45 +0200

    fix start slurm script

--- start_slum_job.sh
+++ start_slum_job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cp ./configs/road_rgb_2D.yaml "./configs/experiments/$1.yaml"
+cp ~/relationformer/configs/road_rgb_2D.yaml ~/relationformer/configs/experiments/"$1.yaml"
 sbatch <<EOT
 #!/bin/sh
-#SBATCH --job-name="$1"
-#SBATCH --output="/home/guests/alexander_berger/slurm_out/$1.out"
-#SBATCH --error="/home/guests/alexander_berger/slurm_out/$1.out"
+#SBATCH --job-name=$1
+#SBATCH --output=/home/guests/alexander_berger/slurm_out/$1.out
+#SBATCH --error=/home/guests/alexander_berger/slurm_out/$1.err
 #SBATCH --time=5-00:00:00
 #SBATCH --cpus-per-task=4  # Number of CPUs (Don't use more than 24 per GPU)
-#--mem=20G  # Memory in GB (Don't use more than 126G per GPU)
+#SBATCH --gres=gpu:1
 #SBATCH --mem-per-gpu=20GB
 
 # load python module
@@ -17,10 +17,10 @@ ml python/anaconda3
 # activate corresponding environment
 conda deactivate # If you launch your script from a terminal where your environment is already loaded, conda won't activate the environment. This guards against that. Not necessary if you always run this script from a clean terminal
 #conda activate relationformer
-source ~/relationformer/env/bin/activate
+source /home/guests/alexander_berger/relationformer/env/bin/activate
 
 #python ~/relationformer/train.py --config ~/relationformer/configs/road_rgb_2D.yaml
 #python ~/relationformer/models/ops/setup.py install
-python ~/relationformer/train.py --config "~/relationformer/configs/experiments/$1.yaml"
+python /home/guests/alexander_berger/relationformer/train.py --config /home/guests/alexander_berger/relationformer/configs/experiments/$1.yaml
 exit 0
 EOT
